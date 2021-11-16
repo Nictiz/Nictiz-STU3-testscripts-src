324d11ef18f763bd72b4855ef61133e4e35ecaf6
Pieter Edelman <edelman@nictiz.nl>
2021-11-16 16:25:06 +0100

    For non-NTS loadscripts, use the default file name

--- centralize-loadscripts.bat
+++ centralize-loadscripts.bat
@@ -12,12 +12,13 @@ if not exist _LoadResources (mkdir _LoadResources)
 
 for /R %%s in (*load-resources-purgecreateupdate*.xml) do (
     set fullPath=%%~dps
+    set fileName=%%~nxs
     set relPath=!fullPath:%cd%=!
     if not "!relPath:~0,16!"=="\_LoadResources\" (
         echo Rewriting %%s
              
         call :normalizeRelPath !relPath! normalizedRelPath
-        call :buildFileName !normalizedRelPath! fileName
+        call :buildFileName !normalizedRelPath! !fileName! fileName
         if exist !fileName! (del !fileName!)
         call :substInFile %%s !normalizedRelPath! !fileName!       
     )
@@ -37,16 +38,21 @@ REM returns: The relative path from the centralized _LoadResources folder to the
     set %~2=..!normalizedPath:/_LoadResources/=!
 exit /b
 
-REM Builds a file name (relative to the working dir) for the new loadscript, based on the folder name. It is assumed
-REM that the leaf dir is the name of the information standard.
+REM If no informative file name exists for the loadscript, build a file name (relative to the working dir) for the new
+REM loadscript, based on the folder name. It is assumed that the leaf dir is the name of the information standard.
 REM param 1: The normalized path as returned by normalizeRelPath.
+REM param 2: The current name of the loadscript file.
 REM returns: The full path to the file name for the new loadscript.
 :buildFileName
     set passedPath=%1
     for %%n in (%passedPath:/= %) do (
         set folderName=%%n
     )
-    set %~2=_LoadResources\%folderName%-load-resources-purgecreateupdate-xml.xml
+    if %2==load-resources-purgecreateupdate-xml.xml (
+        set %~3=_LoadResources\%folderName%-load-resources-purgecreateupdate-xml.xml
+    ) else (
+        set %~3=_LoadResources\%2
+    )
 exit /b
 
 REM Performs the magic of rewriting all reference paths.
